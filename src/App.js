e335fb4042c0660b9a16476a0fdcb70a53c17806
Frank Valdez <fvaldez421@gmail.com>
2018-07-02 15:25:35 -0700

    nav done, working pills and active tab

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
+/*eslint no-restricted-globals: 0 */
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import './App.css';
 
-import Nav from "./components/Nav"
-import Feed from "./pages/Feed"
+import Nav from "./components/Nav";
+import Feed from "./pages/Feed";
 
 class App extends Component {
-    
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
     render() {
         return (
             <div className="App">
                 <Router >
                     <div>
-                        <Nav />
-                        <Route exact path="/" component={Feed} />
+                        <Nav page={location.pathname}/>
+                        <Route exact path="/" render={() => (<Redirect to="/feed" />)} />
+                        <Route path="/feed" component={Feed} />
+                        {/* <Route path="/groups" component={Groups} />
+                        <Route path="/activity" component={Activity} /> */}
                     </div>
                 </Router>
             </div>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,34 +1,79 @@
-import React from 'react';
-// import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 import "./Nav.css";
 
 
-const Nav = (props) => {
-    return (
-        <div className="topnav">
-            <div className="col-md-10 mr-auto ml-auto">
-                <div className="search">
-                    <span className="material-icons full">search</span>
-                </div>
-                <div href="feed" className="tab active">
-                    <span className="material-icons eq">home</span>
-                    <span> Feed</span>
-                </div>
-                <div href="groups" className="tab">
-                    <span className="material-icons eq">explore</span>
-                    <span> Groups</span>
-                </div>
-                <div href="activity" className="tab">
-                    <span className="material-icons eq">notifications</span>
-                    <span> Activity</span>
-                </div>
-                <button className="btn addPost round">Add Post</button>
-                <button className="img round"></button>
-                <input className="round searchBar" value="Search" />
+class Nav extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            search: "",
+            page: "feed"
+        }
+    }
 
-            </div>
-        </div >
-    )
+    onChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
+
+    render() {
+        return (
+            <div className="topnav">
+                <div className="col-md-10 mr-auto ml-auto">
+                    <div className="search">
+                        <span className="material-icons full">search</span>
+                    </div>
+
+                    <Link to="/feed" className="link">
+                        <div className={
+                            window.location.pathname === "/feed" || window.location.pathname === "/" ?
+                                "active tab"
+                                :
+                                "tab"
+                        } >
+                            <span className="material-icons eq">home</span>
+                            <span> Feed</span>
+                        </div>
+                    </Link>
+
+                    <Link to="/groups" className="link">
+                        <div className={
+                            window.location.pathname === "/groups" ?
+                                "active tab"
+                                :
+                                "tab"
+                        } >
+                            <span className="material-icons eq">explore</span>
+                            <span> Groups</span>
+                        </div>
+                    </Link>
+
+                    <Link to="/activity" className="link">
+                        <div className={
+                            window.location.pathname === "/activity" || "" ?
+                                "active tab"
+                                :
+                                "tab"
+                        } >
+                            <span className="material-icons eq">notifications</span>
+                            <span> Activity</span>
+                        </div>
+                    </Link>
+
+                    <button className="btn addPost round">Add Post</button>
+                    <button className="img round"></button>
+                    <input className="round searchBar"
+                        name="search"
+                        placeholder="Search"
+                        value={this.state.search}
+                        onChange={this.onChange}
+                    />
+
+                </div>
+            </div >
+        )
+    }
 }
 
 export default Nav;
