c1a7448b111643dbd7343a7be315964443e0eb74
Frank Valdez <fvaldez421@gmail.com>
2018-07-04 22:09:12 -0700

    modified data flow, added modalStatus to control modal rendering

--- src/App.js
+++ src/App.js
@@ -5,35 +5,45 @@ import { connect } from "react-redux";
 import { fetchData } from "./actions/dataActions";
 import './App.css';
 
+import ModalController from "./pages/ModalController";
 import Nav from "./components/Nav";
 import Feed from "./pages/Feed";
 
 class App extends Component {
 
     componentWillMount() {
-        // this.props.fetchData();
+        this.props.fetchData();
     }
 
     render() {
+        let blurStyle = this.props.modalStatus === "closed" ? "mainContainer" : "mainContainer blurred";
         return (
             <div className="App">
                 <Router >
                     <div>
-                        <Nav user={this.props.users[3]} />
-                        <div className="col-md-12">
-                            <Route exact path="/" render={() => (<Redirect to="/feed" />)} />
-                            <Route path="/feed"
-                                render={() => (
-                                    <Feed
-                                        users={this.props.users}
-                                        user={this.props.users[3]}
-                                        posts={this.props.posts}
-                                        fetchData={this.props.fetchData}
-                                    />
-                                )}
-                            />
-                            {/* <Route path="/groups" component={Groups} />
+                        {
+                            this.props.modalStatus !== "closed" ? 
+                            (<ModalController />)
+                            :
+                            (<div></div>)
+                        }
+                        <div className={blurStyle}>
+                            <Nav user={this.props.users[3]} />
+                            <div className="col-md-12">
+                                <Route exact path="/" render={() => (<Redirect to="/feed" />)} />
+                                <Route path="/feed"
+                                    render={() => (
+                                        <Feed
+                                            users={this.props.users}
+                                            user={this.props.users[3]}
+                                            posts={this.props.posts}
+                                            fetchData={this.props.fetchData}
+                                        />
+                                    )}
+                                />
+                                {/* <Route path="/groups" component={Groups} />
                             <Route path="/activity" component={Activity} /> */}
+                            </div>
                         </div>
                     </div>
                 </Router>
@@ -42,16 +52,18 @@ class App extends Component {
     }
 }
 
-
 App.propTypes = {
     fetchData: PropTypes.func.isRequired,
     users: PropTypes.array.isRequired,
-    posts: PropTypes.array.isRequired
+    posts: PropTypes.array.isRequired,
+    modalStatus: PropTypes.string.isRequired
 }
 
 const mapStateToProps = state => ({
     users: state.data.items.users,
-    posts: state.data.items.posts
+    posts: state.data.items.posts,
+    modalStatus: state.modalStatus.modalStatus
 })
 
-export default connect(mapStateToProps, { fetchData })(App)
+export default connect(mapStateToProps, { fetchData })(App);
+
