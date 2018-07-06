beb818a798229475d7ba05c19d6e0bf3b2b915e2
Frank Valdez <fvaldez421@gmail.com>
2018-07-05 22:04:55 -0700

    added more styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -42,52 +42,56 @@ const Nav = (props) => {
                     <div className="col-md-12">
                         <div className="row">
                             <div className="col-lg-5 pills">
-                                <div className="search">
-                                    <span className="material-icons full">search</span>
+                                <div className="center ml-auto mr-auto">
+                                    <div className="search">
+                                        <span className="material-icons full">search</span>
+                                    </div>
+                                    <Link to="/feed" className="link">
+                                        <div className={
+                                            basePage === "feed" ?
+                                                "active tab"
+                                                :
+                                                "tab"
+                                        } >
+                                            <span className="material-icons eq">home</span>
+                                            <span> Feed</span>
+                                        </div>
+                                    </Link>
+                                    <Link to="/groups" className="link">
+                                        <div className={
+                                            basePage === "groups" ?
+                                                "active tab"
+                                                :
+                                                "tab"
+                                        } >
+                                            <span className="material-icons eq">explore</span>
+                                            <span> Groups</span>
+                                        </div>
+                                    </Link>
+                                    <Link to="/activity" className="link">
+                                        <div className={
+                                            window.location.pathname === "/activity" || "" ?
+                                                "active tab"
+                                                :
+                                                "tab"
+                                        } >
+                                            <span className="material-icons eq">notifications</span>
+                                            <span> Activity</span>
+                                        </div>
+                                    </Link>
                                 </div>
-                                <Link to="/feed" className="link">
-                                    <div className={
-                                        basePage === "feed" ?
-                                            "active tab"
-                                            :
-                                            "tab"
-                                    } >
-                                        <span className="material-icons eq">home</span>
-                                        <span> Feed</span>
-                                    </div>
-                                </Link>
-                                <Link to="/groups" className="link">
-                                    <div className={
-                                        basePage === "groups" ?
-                                            "active tab"
-                                            :
-                                            "tab"
-                                    } >
-                                        <span className="material-icons eq">explore</span>
-                                        <span> Groups</span>
-                                    </div>
-                                </Link>
-                                <Link to="/activity" className="link">
-                                    <div className={
-                                        window.location.pathname === "/activity" || "" ?
-                                            "active tab"
-                                            :
-                                            "tab"
-                                    } >
-                                        <span className="material-icons eq">notifications</span>
-                                        <span> Activity</span>
-                                    </div>
-                                </Link>
                             </div>
                             <div className="col-lg-7">
-                                <button className="btn addPost round FR">Add Post</button>
-                                {user ?
-                                    (<img src={user.attributes.avatar_thumb} alt="sorry" className="round img FR mr-3"></img>)
-                                    :
-                                    (<img src={"https://apinew.pincapp.com/images/default_avatar.png"} alt="sorry" className="round img FR mr-3"></img>)
-                                }
+                                <div className="center ml-auto mr-auto">
+                                    <button className="btn addPost round FR">Add Post</button>
+                                    {user ?
+                                        (<img src={user.attributes.avatar_thumb} alt="sorry" className="round img FR mr-3"></img>)
+                                        :
+                                        (<img src={"https://apinew.pincapp.com/images/default_avatar.png"} alt="sorry" className="round img FR mr-3"></img>)
+                                    }
 
-                                <SearchBar />
+                                    <SearchBar />
+                                </div>
                             </div>
                         </div>
                     </div>
