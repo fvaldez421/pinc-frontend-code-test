61f4a4bb617eb114909c6172da67597be0a4006f
Frank Valdez <fvaldez421@gmail.com>
2018-07-05 20:00:59 -0700

    added new case to switch for reply modal, added dispatch methods and post to props

--- src/pages/ModalController/ModalController.js
+++ src/pages/ModalController/ModalController.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { sendPost } from "./../../actions/dataActions";
+import { sendPost, sendComment } from "./../../actions/dataActions";
 import { closeModal } from "./../../actions/modalActions";
 
 import PostModal from "./../../components/PostModal";
+import CommentModal from "./../../components/CommentModal";
 
 const Modal = (props) => {
     let status = props.modalStatus
@@ -12,42 +13,46 @@ const Modal = (props) => {
         case "NewPost":
             // console.log(status);
             return (<PostModal sendPost={props.sendPost} user={props.users[3]} closeModal={props.closeModal} />)
+        case "Comment":
+            // console.log(status);
+            return (<CommentModal sendComment={props.sendComment} user={props.users[3]} post={props.post} closeModal={props.closeModal} />)
         default:
             // console.log(status);
             return (<div></div>)
-        }
+    }
 }
 
 const ModalController = (props) => {
     // console.log(props);
-    // console.log(props.modalStatus);
     let status = props.modalStatus
 
     return (
         status !== "closed" ?
-            (<Modal {...props}/>)
+            (<Modal {...props} />)
             :
             (<div></div>)
 
     )
 }
 
-
-
 ModalController.propTypes = {
     sendPost: PropTypes.func.isRequired,
+    sendComment: PropTypes.func.isRequired,
     modalStatus: PropTypes.string.isRequired,
+    post: PropTypes.object,
     users: PropTypes.array.isRequired
 }
 
 const mapDispatchToProps = {
     sendPost,
+    sendComment,
     closeModal
 }
 
 const mapStateToProps = (state) => ({
     users: state.data.items.users,
-    modalStatus: state.modalStatus.modalStatus
+    modalStatus: state.modal.modalStatus,
+    post: state.modal.post
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ModalController)
